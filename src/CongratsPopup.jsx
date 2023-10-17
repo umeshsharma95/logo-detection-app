78e9dc5905a6f96df97147dacce2ad8c339ac40c
Umesh Sharma <umeshsharma1495@gmal.com>
2023-10-17 15:48:47 +0530

    lottie speed changes

--- src/CongratsPopup.jsx
+++ src/CongratsPopup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Modal, ModalBody, ModalHeader } from 'reactstrap'
 import Lottie from "react-lottie";
 import congratsFile from './assets/congrats.json'
@@ -13,11 +13,15 @@ const defaultOptions = {
 };
 
 function CongratsPopup({ isOpen, setIsOpen }) {
-  
+  const [speed, setSpeed] = useState(1)
   const toggle = () => {
     setIsOpen(false)
   }
   const closeBtn = (<button type="button" class="btn-close" onClick={toggle} data-bs-dismiss="modal" aria-label="Close"></button>)
+
+  useEffect(() => {
+    setSpeed(isOpen ? 0.5 : 1)
+  }, [isOpen])
 
   return (
     <Modal size='sm' centered isOpen={isOpen} toggle={toggle}>
@@ -27,7 +31,7 @@ function CongratsPopup({ isOpen, setIsOpen }) {
             options={defaultOptions} 
             height={200}
             width={200}
-            speed={0.6}
+            speed={speed}
           />
           <h2 className='my-4 text-center'>Congratulations! you have won 2GB data.</h2>
           <Button onClick={toggle} size='lg' color="danger" className='align-item-center'>Retry</Button>

7ce70050d3d242c4ebe6ae3c1da09c0408014def
Umesh Sharma <umeshsharma1495@gmal.com>
2023-10-17 16:38:27 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ function App() {
   const [imageDetected, setImageDetected] = useState(false);
 
   return (
-    <Container className='appContainer'>
+    <div className='appContainer'>
       {!imageDetected && <MindARViewer setImageDetected={setImageDetected} />}
       <div>
         <CongratsPopup isOpen={imageDetected} setIsOpen={setImageDetected} />
       </div>
-    </Container>
+    </div>
   );
 }
 
